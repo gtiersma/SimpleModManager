b41bc35036e4e5d65bc0564cbda2fbf96a59cae9
George Tiersma <gtiersma@juno.com>
2025-07-06 22:22:48 -0700

    Revert "Fixed issue of 0x1 error. Apparently init always throws it?"
    This reverts commit 97f097e1666ed5a564ac882a8958e7e077b2e6ee.

--- src/Applications/SimpleModManager/src/SimpleModManager.cpp
+++ src/Applications/SimpleModManager/src/SimpleModManager.cpp
@@ -43,7 +43,7 @@ void runGui() {
   alchemyLogger.log("runGui();");
 
   brls::i18n::loadTranslations("en-US");
-  brls::Application::init("SimpleModManager");
+  MetaManager::tryResult(brls::Application::init("SimpleModManager"));
 
   auto* mainFrame = new FrameRoot();
 
