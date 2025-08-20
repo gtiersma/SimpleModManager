a2e3daded425729f6d2cabb0b1cbb26ecdf46593
George Tiersma <gtiersma@juno.com>
2025-08-19 23:06:00 -0700

    Fixed issue of the About tab crashing the app due to the columns box attempting to be added to the main frame twice. Also made many layout adjustment to fix visual glitches.

--- src/ModManagerGui/FrameGameBrowser/src/TabAbout.cpp
+++ src/ModManagerGui/FrameGameBrowser/src/TabAbout.cpp
@@ -6,20 +6,24 @@
 #include <StateAlchemist/constants.h>
 #include <util.hpp>
 
-TabAbout::TabAbout() {
-  Util::padTabContent(this);
+TabAbout::TabAbout(): brls::Box(brls::Axis::COLUMN) {
+  this->setPadding(16.0f, 40.0f, 32.0f, 40.0f);
+  this->setJustifyContent(brls::JustifyContent::SPACE_BETWEEN);
   
   // Subtitle
   brls::Label* shortDescription = new brls::Label();
   shortDescription->setText(
-    "Simple Mod Alchemist is an Nintendo Switch homebrew app for managing mods on the SD card.\n"\
-    "It originated by merging the code of two homebrew apps together: SimpleModManager and State Alchemist.\n"
+    "Simple Mod Alchemist is a Nintendo Switch homebrew app for changing game mods.\n"\
+    "It resulted from the fusion of two other apps: SimpleModManager and State Alchemist.\n"
   );
   shortDescription->setHorizontalAlign(brls::HorizontalAlign::CENTER);
+  shortDescription->setIsWrapping(true);
+  shortDescription->setFontSize(15.0f);
   this->addView(shortDescription);
 
   brls::Box* columns = new brls::Box();
   columns->setJustifyContent(brls::JustifyContent::SPACE_BETWEEN);
+  columns->setHeight(300.0f);
   this->addView(columns);
 
   brls::Box* leftBox = new brls::Box(brls::Axis::COLUMN);
@@ -35,10 +39,12 @@ TabAbout::TabAbout() {
   changelog->setText(
     " - Mods are now grouped\n"\
     " - Major performance improvements with large files\n"\
+    " - Major visual interface upgrade\n"
     " - Improved how mods are organized in folders\n"\
     " - The best part: General stability improvements\n"
   );
   changelog->setHorizontalAlign(brls::HorizontalAlign::LEFT);
+  changelog->setFontSize(15.0f);
   leftBox->addView(changelog);
 
   brls::Header* copyrightHeader = new brls::Header();
@@ -48,29 +54,29 @@ TabAbout::TabAbout() {
   brls::Label* copyright = new brls::Label();
   copyright->setText(
     "Simple Mod Alchemist is licensed under GPL-v3.0\n" \
-        "\u00A9 SimpleModManager 2019 - 2023 Nadrino"\
+        "\u00A9 SimpleModManager 2019 - 2023 Nadrino\n"\
         "\u00A9 Simple Mod Alchemist 2025 gtiersma"
   );
   copyright->setHorizontalAlign(brls::HorizontalAlign::CENTER);
+  copyright->setFontSize(13.0f);
   leftBox->addView(copyright);
 
   brls::Box* rightBox = new brls::Box(brls::Axis::COLUMN);
-  rightBox->setJustifyContent(brls::JustifyContent::SPACE_BETWEEN);
+  rightBox->setJustifyContent(brls::JustifyContent::CENTER);
   rightBox->setWidth(200);
   columns->addView(rightBox);
 
   brls::Image* portrait = new brls::Image();
-  portrait->setImageFromFile("romfs:/images/portrait.jpg");
+  portrait->setImageFromRes("img/portrait.jpg");
   portrait->setScalingType(brls::ImageScalingType::FIT);
   rightBox->addView(portrait);
 
   brls::Label* portraitLabel = new brls::Label();
   portraitLabel->setText("SimpleModManager Original Author: Nadrino");
-  portraitLabel->setFontSize(15.0f);
+  portraitLabel->setFontSize(13.0f);
   portraitLabel->setHorizontalAlign(brls::HorizontalAlign::CENTER);
+  portraitLabel->setMarginTop(4.0f);
   rightBox->addView(portraitLabel);
-
-  this->addView(columns);
 
   brls::Header* creditsHeader = new brls::Header();
   creditsHeader->setTitle("Credits");
@@ -78,14 +84,14 @@ TabAbout::TabAbout() {
 
   brls::Label* credits = new brls::Label();
   credits->setText(
-    "\u00A9  Maintained by gtiersma.\n"\
-    "\u00A9  Built upon SimpleModManager, developed by Nadrino.\n"\
-    "\u00A9  Powered by Borealis, provided by the RetroNX team.\n"\
-    "\u00A9  Special thanks to RetroNX, devkitPro,"\
-    " the ethical homebrew development community in general, and Nintendo.\n"\
-    "\u00A9  This software is NOT licensed by Nintendo.\n"
+    "- Maintained by gtiersma.\n"\
+    "- Built upon SimpleModManager, developed by Nadrino.\n"\
+    "- Powered by Borealis, provided by the RetroNX team.\n"\
+    "- Special thanks to RetroNX, devkitPro, the ethical homebrew development community in general, and Nintendo.\n"\
+    "- This software is unofficial. It is not protected under any license by Nintendo.\n"
   );
-  credits->setFontSize(15.0f);
+  credits->setFontSize(13.0f);
+  credits->setIsWrapping(true);
   this->addView(credits);
 }
 
